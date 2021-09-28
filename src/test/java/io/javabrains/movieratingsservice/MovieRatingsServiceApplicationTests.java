5358c83fe52685a8bde874c0cfd0616545a6baa8
jadhavp12 <jadhav.p12@gmail.com>
2021-09-28 23:00:06 +0530

    first commit

---
+++ src/test/java/io/javabrains/movieratingsservice/MovieRatingsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.movieratingsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRatingsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
