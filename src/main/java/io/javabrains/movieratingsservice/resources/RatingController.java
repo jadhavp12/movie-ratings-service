75924a3d3aa129713410a17ee03fac8a6becfa49
jadhavp12 <jadhav.p12@gmail.com>
2021-09-29 22:17:51 +0530

    call movie-ratings-service via resttemplate

--- src/main/java/io/javabrains/movieratingsservice/resources/RatingController.java
+++ src/main/java/io/javabrains/movieratingsservice/resources/RatingController.java
@@ -1,9 +1,13 @@
 package io.javabrains.movieratingsservice.resources;
 
 import io.javabrains.movieratingsservice.model.Rating;
+import io.javabrains.movieratingsservice.model.UserRating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping("/rating")
@@ -12,6 +16,19 @@ public class RatingController {
     @RequestMapping("/{movieId}")
     public Rating getRating(@PathVariable ("movieId") String movieId){
         return new Rating(movieId,4);
+    }
+
+    @RequestMapping("users/{userId}")
+    public UserRating getUserRating(@PathVariable ("userId") String userId){
+
+        List<Rating> ratings= Arrays.asList(
+                new Rating("1234",4),
+                new Rating("5678",3)
+        );
+        UserRating userRating=new UserRating();
+        userRating.setUserRating(ratings);
+
+        return userRating;
     }
 
 }
